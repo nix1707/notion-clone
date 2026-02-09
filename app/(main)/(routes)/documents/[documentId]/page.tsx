d13e68637eb07b97ccd852af88d6ea79afc5c716
nix1707 <user58490185@gmail.com>
2026-02-10 00:26:11 +0200

    added emoji picker and title inside document

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import Toolbar from "@/components/toolbar";
+import { useParams } from "next/navigation";
 
 const DocumentIdPage = () => {
+  const params = useParams();
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<"documents">,
+  });
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
   return (
-    <div>DocumentId</div>
-  )
-}
+    <div className="pb-40 ">
+      <div className="h-[35vh] " />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
+};
 
 export default DocumentIdPage;
